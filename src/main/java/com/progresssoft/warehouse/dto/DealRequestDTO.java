aaaf00beb7d6b77c9db0dac8c8ec749082b5a8ee
safia-khoulaid <safiakhoulaid11@gmail.com>
2026-02-05 20:31:10 +0000

    FEAT(CW-12): Add DealRequestDTO record with validation constraints

---
+++ src/main/java/com/progresssoft/warehouse/dto/DealRequestDTO.java
@@ -0,0 +1,30 @@
+package com.progresssoft.warehouse.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record DealRequestDTO(
+
+        @NotBlank(message = "Deal Unique ID is mandatory")
+        String dealUniqueId,
+
+        @NotBlank(message = "From currency ISO code is mandatory")
+        @Size(min = 3, max = 3, message = "From currency ISO code must be exctly 3 characters")
+        String fromCurrencyIsoCode,
+
+        @NotBlank(message = "To currency ISO code is mandatory")
+        @Pattern(regexp = "^[A-Z]{3}$", message = "To currency ISO code must be exactly 3 uppercase letters") String toCurrencyIsoCode,
+
+        @NotNull(message = "Deal timestamp is mandatory")
+        @PastOrPresent(message = "Deal timestamp cannot be in the future")
+        Instant dealTimestamp,
+
+        @NotNull(message = "Amount is mandatory")
+        @Positive(message = "Amount must be a positive value")
+        BigDecimal dealAmount
+
+
+) {
+}
