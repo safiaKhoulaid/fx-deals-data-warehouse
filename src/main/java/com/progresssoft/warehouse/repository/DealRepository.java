2c44518e5ca2ca5dc67efefc8840cc892b3d0abe
safia-khoulaid <safiakhoulaid11@gmail.com>
2026-02-06 16:11:00 +0000

    REFACTOR: Add method to find existing deal unique IDs in the repository

--- src/main/java/com/progresssoft/warehouse/repository/DealRepository.java
+++ src/main/java/com/progresssoft/warehouse/repository/DealRepository.java
@@ -2,12 +2,20 @@ package com.progresssoft.warehouse.repository;
 
 import com.progresssoft.warehouse.model.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface DealRepository extends JpaRepository<Deal, Long> {
 
     boolean existsByDealUniqueId(String dealUniqueId);
+
+    @Query("SELECT d.dealUniqueId FROM Deal d WHERE d.dealUniqueId IN :ids")
+    Set<String> findExistingIds(@Param("ids") List<String> ids);
 
 
 }
