2fc49ca3b3cfc4f06e4e0873956ad369c775b26b
safia-khoulaid <safiakhoulaid11@gmail.com>
2026-02-06 15:35:57 +0000

    FEAT(CW-18): Add validation annotations to DealRequestDTO and implement GlobalExceptionHandler for error handling

--- src/main/java/com/progresssoft/warehouse/dto/DealRequestDTO.java
+++ src/main/java/com/progresssoft/warehouse/dto/DealRequestDTO.java
@@ -1,10 +1,13 @@
 package com.progresssoft.warehouse.dto;
 
+import com.progresssoft.warehouse.validation.annotations.DifferentCurrencies;
+import com.progresssoft.warehouse.validation.annotations.IsoCode;
 import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
+@DifferentCurrencies
 public record DealRequestDTO(
 
         @NotBlank(message = "Deal Unique ID is mandatory")
@@ -12,10 +15,12 @@ public record DealRequestDTO(
 
         @NotBlank(message = "From currency ISO code is mandatory")
         @Size(min = 3, max = 3, message = "From currency ISO code must be exctly 3 characters")
+        @IsoCode
         String fromCurrencyIsoCode,
 
         @NotBlank(message = "To currency ISO code is mandatory")
         @Pattern(regexp = "^[A-Z]{3}$", message = "To currency ISO code must be exactly 3 uppercase letters")
+        @IsoCode
         String toCurrencyIsoCode,
 
         @NotNull(message = "Deal timestamp is mandatory")
---
+++ src/main/java/com/progresssoft/warehouse/exception/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.progresssoft.warehouse.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.net.URI;
+import java.time.Instant;
+
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ProblemDetail handleJsonError(HttpMessageNotReadableException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                "JSON structure is invalid. Check for special characters like '*'."
+        );
+        problemDetail.setTitle("Malformed JSON Request");
+        problemDetail.setType(URI.create("https://api.warehouse.com/errors/bad-request"));
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ProblemDetail handleFileSizeError(MaxUploadSizeExceededException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.PAYLOAD_TOO_LARGE,
+                "The uploaded file is too large."
+        );
+        problemDetail.setTitle("File Size Exceeded");
+        problemDetail.setProperty("maxSize", "2MB"); // مـثال
+        return problemDetail;
+    }
+
+
+    @ExceptionHandler(RuntimeException.class)
+    public ProblemDetail handleRuntimeException(RuntimeException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage()
+        );
+        problemDetail.setTitle("Execution Error");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleGlobalError(Exception ex) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "An unexpected internal error occurred."
+        );
+    }
+}
