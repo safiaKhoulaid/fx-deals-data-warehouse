209eabda3f90611a7091e75a5eb6b65dfc179798
safia-khoulaid <safiakhoulaid11@gmail.com>
2026-02-06 13:58:36 +0000

    FEAT: Modify importDeals method to return ImportReportDTO with processing results

--- src/main/java/com/progresssoft/warehouse/service/impl/DealServiceImpl.java
+++ src/main/java/com/progresssoft/warehouse/service/impl/DealServiceImpl.java
@@ -2,6 +2,7 @@ package com.progresssoft.warehouse.service.impl;
 
 
 import com.progresssoft.warehouse.dto.DealRequestDTO;
+import com.progresssoft.warehouse.dto.ImportReportDTO;
 import com.progresssoft.warehouse.mapper.DealMapper;
 import com.progresssoft.warehouse.model.Deal;
 import com.progresssoft.warehouse.repository.DealRepository;
@@ -10,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
@@ -21,8 +24,13 @@ public class DealServiceImpl implements IDealService {
     private final DealMapper dealMapper;
 
     @Override
-    public void importDeals(List<DealRequestDTO> deals) {
+    public ImportReportDTO importDeals(List<DealRequestDTO> deals) {
         log.info("Starting processing of {} deals", deals.size());
+
+        AtomicInteger total = new AtomicInteger(deals.size());
+        AtomicInteger sucess = new AtomicInteger();
+        AtomicInteger duplicates = new AtomicInteger();
+        List<DealRequestDTO> failedDeals = new ArrayList<>();
 
         deals.forEach(dto -> {
 
@@ -30,6 +38,7 @@ public class DealServiceImpl implements IDealService {
 
                 if (dealRepository.existsByDealUniqueId(dto.dealUniqueId())) {
                     log.warn("Deal with ID {} already exists. Skipping...", dto.dealUniqueId());
+                    duplicates.getAndIncrement();
                     return;
                 }
 
@@ -37,13 +46,23 @@ public class DealServiceImpl implements IDealService {
 
                 dealRepository.save(deal);
 
+                sucess.getAndIncrement();
+
                 log.info("Successfully saved deal: {}", dto.dealUniqueId());
 
 
             } catch (Exception e) {
                 log.error("Failed to save deal {}: {}", dto.dealUniqueId(), e.getMessage());
+                failedDeals.add(dto);
             }
         });
         log.info("Finished processing batch.");
+        return new ImportReportDTO(
+                total,
+                sucess,
+                duplicates,
+                failedDeals
+
+        );
     }
 }
