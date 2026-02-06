f282377a1fc75c83186fdeed6322a6c87fdbb3d2
safia-khoulaid <safiakhoulaid11@gmail.com>
2026-02-06 15:36:53 +0000

    FEAT(CW-18): Add InvalidDealException class for handling invalid deal scenarios

---
+++ src/main/java/com/progresssoft/warehouse/exception/customException/InvalidDealException.java
@@ -0,0 +1,5 @@
+package com.progresssoft.warehouse.exception.customException;
+
+public class InvalidDealException extends RuntimeException {
+    public InvalidDealException(String message) { super(message); }
+}
